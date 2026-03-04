e28504d851186f6904a70890d960354b0b97476b
HoangDZ <ddhoang2440@gmail.com>
2026-03-04 20:45:57 +0700

    style: expand corridors to 2-tiles for smoother movement

---
+++ data/tilesets/TilesetFloor.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding ="UTF-8"?>
+    <tileset version="1.10" tiledversion="1.11.2" name="TilesetFloor" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+        <grid orientation="orthogonal" width="32" height="32" />
+        <image source="../graphics/tilesets/TilesetFloor.png" width="352" height="417" />
+    </tileset>
---
+++ data/tilesets/TilesetNature.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+    <tileset version="1.10" tiledversion="1.11.2" name="TilesetNature" tilewidth="16" tileheight="16" tilecount="504" columns="24">
+        <image source="../graphics/tilesets/TilesetNature.png" width="384" height="336" />
+        <tile id="360">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="361">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="362">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="363">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="384">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="385">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="386">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+        <tile id="387">
+            <properties>
+                <property name="type" value="power" />
+            </properties>
+        </tile>
+    </tileset>
